f3c80d8d322942709b5efc5d7bb39a93afdfd42d
OvidijusV <ovidijusv14@gmail.com>
2022-10-26 19:33:24 +0300

    init

---
+++ Parduotuve/UseCases/ViewCategoriesUseCase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UseCases
+{
+    public class ViewCategoriesUseCase
+    {
+        public IEnumerable<Category> Execute()
+    }
+}
