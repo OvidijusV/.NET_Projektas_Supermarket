ec7e4625ec31929d81b827b1e6ca1b12b64e4db8
OvidijusV <ovidijusv14@gmail.com>
2022-11-10 15:13:10 +0200

    add product

--- Parduotuve/Plugins.DataStore.InMemory/ProductInMemoryRepository.cs
+++ Parduotuve/Plugins.DataStore.InMemory/ProductInMemoryRepository.cs
@@ -26,6 +26,24 @@ namespace Plugins.DataStore.InMemory
 
             };
         }
+
+        public void AddProduct(Product product)
+        {
+            if (products.Any(x => x.Name.Equals(product.Name, StringComparison.OrdinalIgnoreCase))) return;
+
+            if (products != null && products.Count > 0)
+            {
+                var maxId = products.Max(x => x.ProductId);
+                product.ProductId = maxId + 1;
+            }
+            else
+            {
+                product.ProductId = 1;
+            }
+
+            products.Add(product);
+        }
+
         public IEnumerable<Product> GetProducts()
         {
             return products;
